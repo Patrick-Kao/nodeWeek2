1ed28e536679a1e140e8862a4a697314892a8976
育祥 <a468223@gmail.com>
2023-04-09 11:31:25 +0900

    作業完成

---
+++ server.js
@@ -0,0 +1,90 @@
+const http = require('http');
+const mongoose = require('mongoose');
+const Posts = require('./modules/post');
+const headers = require('./headers');
+const handleSuccess = require('./handleSuccess')
+const handleError = require('./handleError')
+
+mongoose
+    .connect("mongodb://localhost:27017/testPost")
+    .then(() => console.log('資料庫連接成功'));
+const requestListener = async (req, res) => {
+
+    let body = "";
+    req.on('data', chunk => {
+        body += chunk;
+    })
+    if (req.url == "/posts" && req.method == "GET") {
+        const allPosts = await Posts.find();
+        handleSuccess(res, allPosts);
+
+    } else if (req.url == "/posts" && req.method == "POST") {
+        req.on('end', async () => {
+            try {
+                const data = JSON.parse(body);
+                if (data.name !== undefined && data.content !== undefined) {
+                    const newPost = await Posts.create(
+                        {
+                            name: data.name,
+                            content: data.content,
+                            tags: data.tags,
+                            image: data.image,
+                            likes: data.likes,
+                            comments: data.comments
+                        }
+                    );
+                    handleSuccess(res, newPost);
+                } else {
+                    handleError(res)
+                }
+
+            } catch (err) {
+                handleError(res, err)
+            }
+        })
+
+
+    } else if (req.method == 'OPTIONS') {
+        res.writeHead(200, headers);
+        res.end();
+    } else if (req.url.includes("/posts") && req.method == 'PATCH') {
+
+
+        req.on('end', async () => {
+            try {
+                const data = JSON.parse(body);
+                const id = decodeURIComponent(req.url.slice(7))
+                const patch = await Posts.findByIdAndUpdate(id, {
+                    name: data.name,
+                    content: data.content,
+                    tags: data.tags,
+                    image: data.image,
+                    likes: data.likes,
+                    comments: data.comments
+
+                })
+                handleSuccess(res, patch);
+
+            } catch (err) {
+                handleError(res, err)
+            }
+        })
+    } else if (req.url.includes("/posts") && req.method == 'DELETE') {
+        req.on('end', async () => {
+            try {
+                const data = JSON.parse(body);
+                console.log(data)
+                const id = decodeURIComponent(req.url.slice(7))
+                console.log(id)
+                const patch = await Posts.deleteOne({ _id: id })
+                handleSuccess(res, patch);
+
+            } catch (err) {
+                handleError(res, err)
+            }
+        })
+    }
+
+}
+const server = http.createServer(requestListener);
+server.listen(3000);
